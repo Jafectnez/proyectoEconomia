5b956ea55c98acf1ad2ffe03f731307cf5bf4080
zadenower <jafectnez@gmail.com>
2020-03-01 18:09:13 -0600

    Base datos externa MySQL (temporal)

--- config/app.php
+++ config/app.php
@@ -175,7 +175,10 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /*
+         * Firebase
+         */
+        Kreait\Laravel\Firebase\ServiceProvider::class
     ],
 
     /*
